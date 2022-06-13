b8caf70ef6404793764660c613c4703d1ff0a8f3
Josué Santos <josue.santos.dev@gmail.com>
2022-06-13 16:56:27 -0300

    :package: add clucie

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject tapir "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [clucie "0.4.2"]]
   :main ^:skip-aot tapir.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
